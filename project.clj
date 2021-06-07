9b4b8bd03f7514f61255e1dbab3e4373527d366f
Punit Naik <naik.punit44@gmail.com>
2021-06-07 10:20:31 +0530

    converted into cljc project

--- project.clj
+++ project.clj
@@ -1,47 +1,14 @@
 (defproject vega-viz "1.0.0"
-  :description "Data visualisation library for ClojureScript using Vega-JS"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]
-                 [reagent "0.8.1"]
-                 [funcool/promesa "1.9.0"]
-                 [cljsjs/vega-tooltip "0.6.1-0"]
-                 [cljsjs/vega-embed "3.1.1-0"]
-                 [cljsjs/vega "3.0.1-0"]
-                 [cljsjs/vega-lite "2.0.0-0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.866"]
+                 ;; Vega
+                 [cljsjs/vega-lite "4.17.0-0"]
+                 [cljsjs/vega "5.17.0-0"]
+                 [cljsjs/vega-tooltip "0.24.2-0"]
+                 [cljsjs/vega-embed "6.14.2-0"]
+                 [reagent "1.1.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.7"]]
-
-  :min-lein-version "2.5.3"
-
-  :source-paths ["src/clj" "src/cljs"]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-
-  :figwheel {:css-dirs ["resources/public/css"]}
-
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.10"]]
-    :plugins      [[lein-figwheel "0.5.16"]]}
-   :prod {}}
-
-  :cljsbuild
-  {:builds
-   [{:id           "dev"
-     :source-paths ["src/cljs"]
-     :compiler     {:main                 vega-viz.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
-                    :source-map-timestamp true
-                    :preloads             [devtools.preload]
-                    :external-config      {:devtools/config {:features-to-install :all}}}}
-
-
-    {:id           "min"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            vega-viz.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}]})
+  :source-paths ["src/cljc"]
+  :test-paths ["test"]
+  :aliases {"testing" ["do" ["test"] ["install"]]
+            "build" ["install"]})
